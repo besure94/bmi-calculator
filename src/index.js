880f2ff3b3640fc5bd02ebfcb6cab623e0c84b61
Brian Scherner <brianscherner@gmail.com>
2024-01-16 19:39:16 -0800

    Add functionality to calculate BMI.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+function calculateBmi() {
+  const heightInFeet = document.querySelector("#feet").value;
+  console.log(heightInFeet);
+  const heightInInches = document.querySelector("#inches").value;
+  console.log(heightInInches);
+  const weightInPounds = document.querySelector("#pounds").value;
+  const feetToInches = heightInFeet * 12;
+  console.log(feetToInches);
+  const heightTotal = parseInt(feetToInches) + parseInt(heightInInches);
+  console.log(heightTotal);
+  const bmiResult = (weightInPounds / heightTotal**2) * 703;
+  document.querySelector("p#showCalculation").innerText = `Your BMI is ${bmiResult}.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  calculateBmi();
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form").addEventListener("submit", handleFormSubmission);
+});
