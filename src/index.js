aff0c1e4484a8d915707f79b4d34bc71c228d7d1
Brian Scherner <brianscherner@gmail.com>
2024-01-16 19:39:49 -0800

    Remove console logs.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import './css/styles.css';
 
 function calculateBmi() {
   const heightInFeet = document.querySelector("#feet").value;
-  console.log(heightInFeet);
   const heightInInches = document.querySelector("#inches").value;
-  console.log(heightInInches);
   const weightInPounds = document.querySelector("#pounds").value;
   const feetToInches = heightInFeet * 12;
-  console.log(feetToInches);
   const heightTotal = parseInt(feetToInches) + parseInt(heightInInches);
-  console.log(heightTotal);
   const bmiResult = (weightInPounds / heightTotal**2) * 703;
   document.querySelector("p#showCalculation").innerText = `Your BMI is ${bmiResult}.`;
 }
