3f8ee9801155a1f85ad35fe9ae599c98ed80b572
Brian Scherner <brianscherner@gmail.com>
2024-01-22 10:16:37 -0800

    Add functionality to hide previous BMI results after resetting forms.

--- src/index.js
+++ src/index.js
@@ -2,74 +2,67 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-function displayForm() {
-  const chooseSystem = document.querySelector("input[name='choice']:checked").value;
-  const imperialDiv = document.getElementById("imperialDiv");
-  const metricDiv = document.getElementById("metricDiv");
-  if (chooseSystem == "imperial") {
-    imperialDiv.removeAttribute("class", "hidden");
-    metricDiv.setAttribute("class", "hidden");
-  } else if (chooseSystem == "metric") {
-    metricDiv.removeAttribute("class", "hidden");
-    imperialDiv.setAttribute("class", "hidden");
-  }
+function hideBmiResultsOnImperialFormReset() {
+  document.querySelector("p#showCalculationImperial").setAttribute("class", "hidden");
 }
 
-function calculateBmiImperialSystem() {
-  const heightInFeet = document.querySelector("#feet").value;
-  const heightInInches = document.querySelector("#inches").value;
-  const weightInPounds = document.querySelector("#pounds").value;
-  const feetToInches = heightInFeet * 12;
-  const heightTotal = parseInt(feetToInches) + parseInt(heightInInches);
-  const bmiResult = ((weightInPounds / heightTotal**2) * 703).toFixed(1);
-  document.querySelector("p#showCalculationImperial").innerText = `Your BMI is ${bmiResult}.`;
+function hideBmiResultsOnMetricFormReset() {
+  document.querySelector("p#showCalculationMetric").setAttribute("class", "hidden");
 }
 
 function resetImperialSystemForm() {
   document.getElementById("imperialSystem").reset();
-  // document.querySelector("p#showCalculationImperial").innerText = "";
-}
-
-function calculateBmiMetricSystem() {
-  const heightInCm = document.querySelector("#centimeters").value;
-  const weightinKg = document.querySelector("#kilograms").value;
-  const calculateBmi = ((weightinKg / heightInCm**2) * 10000).toFixed(1);
-  document.querySelector("p#showCalculationMetric").innerText = `Your BMI is ${calculateBmi}.`;
 }
 
 function resetMetricSystemForm() {
   document.getElementById("metricSystem").reset();
 }
 
-function handleUserChoiceFormSubmission(event) {
-  event.preventDefault();
-  displayForm();
-}
-
-function handleImperialFormSubmission(event) {
-  event.preventDefault();
-  calculateBmiImperialSystem();
-}
-
-function resetImperialSystemFormSubmission(event) {
-  event.preventDefault();
-  resetImperialSystemForm();
-}
-
-function handleMetricFormSubmission(event) {
-  event.preventDefault();
-  calculateBmiMetricSystem();
-}
-
-function resetMetricSystemFormSubmission(event) {
-  event.preventDefault();
-  resetMetricSystemForm();
-}
-
 window.addEventListener("load", function() {
-  document.querySelector("form#chooseSystem").addEventListener("submit", handleUserChoiceFormSubmission);
-  document.querySelector("form#imperialSystem").addEventListener("submit", handleImperialFormSubmission);
-  document.querySelector("form#metricSystem").addEventListener("submit", handleMetricFormSubmission);
-  document.querySelector("form#imperialSystem").addEventListener("submit", resetImperialSystemFormSubmission());
-  document.querySelector("form#metricSystem").addEventListener("submit", resetMetricSystemFormSubmission());
+  document.querySelector("form#chooseSystem").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const chooseSystem = document.querySelector("input[name='choice']:checked").value;
+    const imperialDiv = document.getElementById("imperialDiv");
+    const metricDiv = document.getElementById("metricDiv");
+    if (chooseSystem == "imperial") {
+      imperialDiv.removeAttribute("class", "hidden");
+      metricDiv.setAttribute("class", "hidden");
+    } else if (chooseSystem == "metric") {
+      metricDiv.removeAttribute("class", "hidden");
+      imperialDiv.setAttribute("class", "hidden");
+    }
+  });
+
+  document.querySelector("form#imperialSystem").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const heightInFeet = document.querySelector("#feet").value;
+    const heightInInches = document.querySelector("#inches").value;
+    const weightInPounds = document.querySelector("#pounds").value;
+    const feetToInches = heightInFeet * 12;
+    const heightTotal = parseInt(feetToInches) + parseInt(heightInInches);
+    const bmiResult = ((weightInPounds / heightTotal**2) * 703).toFixed(1);
+    document.querySelector("p#showCalculationImperial").removeAttribute("class", "hidden");
+    document.querySelector("p#showCalculationImperial").innerText = `Your BMI is ${bmiResult}.`;
+
+    document.getElementById("resetForm").addEventListener("click", function (resetEvent) {
+      resetEvent.preventDefault();
+      resetImperialSystemForm();
+      hideBmiResultsOnImperialFormReset();
+    });
+  });
+
+  document.querySelector("form#metricSystem").addEventListener("submit", function(submitMetricEvent) {
+    submitMetricEvent.preventDefault();
+    const heightInCm = document.querySelector("#centimeters").value;
+    const weightinKg = document.querySelector("#kilograms").value;
+    const calculateBmi = ((weightinKg / heightInCm**2) * 10000).toFixed(1);
+    document.querySelector("p#showCalculationMetric").removeAttribute("class", "hidden");
+    document.querySelector("p#showCalculationMetric").innerText = `Your BMI is ${calculateBmi}.`;
+
+    document.getElementById("resetButton").addEventListener("click", function(resetMetricEvent) {
+      resetMetricEvent.preventDefault();
+      resetMetricSystemForm();
+      hideBmiResultsOnMetricFormReset();
+    });
+  });
 });
